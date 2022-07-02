62bdb63ed2f2482cd996b55663b7eed192e13b9f
“MaHu4” <marenhuser@gmail.com>
2022-07-02 09:08:53 +0200

    before userAge is added

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class SampleForm(FlaskForm):
     userName = StringField('Your name', validators=[DataRequired(), Length(min=1, max=80)]) # Requirements for userName: A string mus be entered (StringField); something must be entered (DataRequired), length; STring-field can be replaced by select field etc
-    userAge = SelectField('Your Age', validators=[DataRequired(), Length(min=1, max=2)])
+    # userAge = IntegerField('Your Age', validators=[DataRequired()])
+    # # email = StringField('Your Email', [Email(message=('Not a valid email address.'))DataRequired()]
 
     submit = SubmitField('Submit')
